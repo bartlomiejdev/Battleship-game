b4e467145463c57729dde8de962757810a98e201
bartlomiejdev <rzeznia777@gmail.com>
2023-03-04 13:02:04 +0000

    Set up players and computer board. Print out the boards

--- run.py
+++ run.py
@@ -53,3 +53,22 @@ def main():
     name = input("What is your name? ")
     print(f"Hello {name}! Let's play Battleship.\n")
     board_size_user_choice = int(input("Choose size of grid from 1-9: \n"))
+
+    # Set up players board
+    board_size = board_size_user_choice
+    player_board = []
+    for _ in range(board_size):
+        player_board.append(["."] * board_size)
+    place_ships(player_board)
+
+    # Set up computers board
+    computer_board = []
+    for i in range(board_size):
+        computer_board.append(["."] * board_size)
+    place_ships(computer_board)
+
+    # Print the board
+    print("This is your board:\n")
+    print_board(player_board)
+    print("\nThis is computers board:\n")
+    print_board(computer_board)
