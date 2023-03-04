1de18bb1b9086eaec19e0cecdd1df4cb428a8828
bartlomiejdev <rzeznia777@gmail.com>
2023-03-04 12:07:05 +0000

    Create function that print out the board game

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def print_board(board):
+    print("  ", end="")
+    for i in range(len(board[0])):
+        print(str(i+1), end=" ")
+    print()
+    for i in range(len(board)):
+        row_str = str(i + 1) + " "
+        for j in range(len(board[i])):
+            if board[i][j] == ".":
+                row_str += ". "
+            else:
+                row_str += board[i][j] + " "
+        print(row_str)
