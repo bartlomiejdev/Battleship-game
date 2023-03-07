9205cb4e15bcfc1180c3b8c9175db6a0cadff752
bartlomiejdev <rzeznia777@gmail.com>
2023-03-07 18:20:43 +0000

    Add main function of the game and fix bug where grid function never returned any value

--- run.py
+++ run.py
@@ -59,14 +59,14 @@ def grid_size():
         if (board_size_user_choice.isdigit() and
                 1 <= int(board_size_user_choice)) <= 9:
             board_size_user_choice = int(board_size_user_choice)
-            break
+            return board_size_user_choice
         else:
             print("Invalid input. Please enter a number between 1 and 9.")
 
 
 def set_up_player_board(grid_size):
-    '''Set ups the player board as a list based on the value returned from grid_size()'''
-    # Set up players board
+    '''Set ups the player board as a list based on the value returned
+     from grid_size()'''
     board_size = grid_size
     player_board = []
     for _ in range(board_size):
@@ -76,7 +76,8 @@ def set_up_player_board(grid_size):
 
 
 def set_up_computer_board(grid_size):
-    '''Set ups the computer board as a list based on the value returned from grid_size()'''
+    '''Set ups the computer board as a list based on the value returned
+     from grid_size()'''
     # Set up computer board
     board_size = grid_size
     computer_board = []
@@ -192,93 +193,16 @@ def print_the_final_board(player_board, computer_board):
     print_board(computer_board, False)
 
 
-# def main():
-#     '''Function that ask for name, sets up
-#      the boards, lopping game and set the turns'''
-#     # Get the user's name
-
-#     while True:
-#         board_size_user_choice = input("Choose size of grid from 1-9: \n")
-#         if (board_size_user_choice.isdigit() and
-#                 1 <= int(board_size_user_choice)) <= 9:
-#             board_size_user_choice = int(board_size_user_choice)
-#             break
-#         else:
-#             print("Invalid input. Please enter a number between 1 and 9.")
-
-#     # Set up players board
-#     board_size = board_size_user_choice
-#     player_board = []
-#     for _ in range(board_size):
-#         player_board.append(["."] * board_size)
-#     place_ships(player_board, board_size)
-
-#     # Set up computers board
-#     computer_board = []
-#     for _ in range(board_size):
-#         computer_board.append(["."] * board_size)
-#     place_ships(computer_board, board_size)
-
-#     # Game loop
-#     computer_ships = board_size - 1
-#     player_ships = board_size - 1
-#     while computer_ships > 0 and player_ships > 0:
-
-#         # Print the board after each round
-#         print("\nThis is your board:")
-#         print_board(player_board, False)
-#         print("\nThis is computer's board:")
-#         print_board(computer_board, True)
-
-#         # User's turn
-#         print("\nYour turn.")
-#         while True:
-#             guess_row = input(f"Guess Row (1-{board_size}): ")
-#             guess_col = input(f"Guess Col (1-{board_size}): ")
-
-#             if not guess_row.isdigit() or not guess_col.isdigit():
-#                 print("Please enter a valid integer.")
-#                 continue
-#             guess_row = int(guess_row) - 1
-#             guess_col = int(guess_col) - 1
-#             if (guess_row < 0 or guess_row >= board_size or
-#                     guess_col < 0 or guess_col >= board_size):
-#                 print(f"Please enter integers in the range 1-{board_size}.")
-#                 continue
-#             break
-
-#         guess = (guess_row, guess_col)
-#         if check_guess(guess, computer_board):
-#             computer_ships -= 1
-#             print("Congratulations! You sunk a battleship!")
-#             if computer_ships == 0:
-#                 print(f"\nCongratulations, {name}! You won!")
-#                 break
-#         else:
-#             print("You missed.")
-
-#         # Computer turn
-#         print("\nComputer's turn.")
-#         while True:
-#             comp_guess_row = random.randint(0, board_size - 1)
-#             comp_guess_col = random.randint(0, board_size - 1)
-#             comp_guess = (comp_guess_row, comp_guess_col)
-#             if (player_board[comp_guess_row][comp_guess_col] != "M" and
-#                     player_board[comp_guess_row][comp_guess_col] != "X"):
-#                 break
-#         if check_guess(comp_guess, player_board):
-#             player_ships -= 1
-#             print("Oh no! The computer sunk one of your battleships!")
-#             if player_ships == 0:
-#                 print("Computer hit your last ship! You lose!")
-#         else:
-#             print("The computer missed.")
-
-#     # Print the final board
-#     print("\nThis is your final board:\n")
-#     print_board(player_board, False)
-#     print("\nThis is computer final board:\n")
-#     print_board(computer_board, False)
+def main():
+    '''Main function that runs the game by calling out the other functions'''
+    name = get_user_name()
+    board_size = grid_size()
+    player_board = set_up_player_board(board_size)
+    computer_board = set_up_computer_board(board_size)
+    ships = game_loop(board_size, name)
+    player_turn(board_size, computer_board, name, ships)
+    computer_turn(board_size, player_board, ships)
+    print_the_final_board(player_board, computer_board)
 
 
 if __name__ == "__main__":
